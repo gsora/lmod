3541166ef37944aa985d65c0e26ec5e32e7d88e5
Gianguido <gianguidorama@gmail.com>
2015-10-06 10:05:35 +0200

    go-style error handling instead of a dirty os.Exit(1)

--- LoadedModules.go
+++ LoadedModules.go
@@ -2,22 +2,22 @@ package lmod
 
 import (
     "io/ioutil"
-    "os"
+    "fmt"
 )
 
-func LoadedModules() ([]string) {
+func LoadedModules() ([]string, error) {
 
     var modules []string
 
     flist, err := ioutil.ReadDir("/sys/module")
 
     if err != nil {
-        os.Exit(1)
+        return modules, fmt.Errorf("lmod: cannot access loaded module list") 
     }
 
     for _,element := range flist {
         modules = append(modules, element.Name())
     }
 
-    return modules
+    return modules, nil
 }
